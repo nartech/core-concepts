4364247eef940b8c6d787c9698ab7b4bc884c38a
Narasimha <nartemp@gmail.com>
2015-04-26 16:22:33 +0530

    add a comment

--- src/org/katakam/corejava/designpatterns/SingletonChildB.java
+++ src/org/katakam/corejava/designpatterns/SingletonChildB.java
@@ -1,7 +1,8 @@
 package org.katakam.corejava.designpatterns;
 
 
-
+//Extending a singleton can defeat the original purpose of singleton by 
+//exposing the sub-class constructor as public
 public class SingletonChildB extends SingletonBaseA{
 
 	private static SingletonChildB  aSingletonBaseA = new SingletonChildB();
